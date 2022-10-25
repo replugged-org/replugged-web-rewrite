4b583a2fda7713ec796139ed25287ae22af56c7d
Alyxia Sother <alyxia@riseup.net>
2022-10-25 10:07:48 +0000

    [codespaces] Add some clauses

--- init-codespace.sh
+++ init-codespace.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-cp .env.example .env
+if [[ ! -f ".env" ]]; then
+    cp .env.example .env
+fi
 
 composer install
 
-php artisan key:generate
+grep -q "base64:.*=" ./.env; i=$(($?));
+if [[ $i != "0" ]]; then
+    php artisan key:generate
+fi
+
 php artisan migrate:fresh --seed
 
 npm install
