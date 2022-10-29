f2e8fa7368ed0ec8022d5636ae77916877e81b0c
Alyxia Sother <alyxia@riseup.net>
2022-10-29 13:30:11 +0000

    [views/layouts/app] Improve the default layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,46 @@
-{{-- TODO: Expand --}}
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="ltr" @yield('htmlExt')>
 
 <head>
+    <title>
+        @hasSection('title')
+            @yield('title') • {{ Config::get('app.name') }}
+        @else
+            {{ Config::get('app.name') }}
+        @endif
+    </title>
+
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="robots" content="noindex, nofollow, noarchive" />
+    <meta name="referrer" content="never" />
+    <meta name="referrer" content="no-referrer" />
+
+    <link rel="icon" type="image/png" href="/build/images/replugged.png" />
+
+    @yield('head')
+
+    @stack('css')
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
 
-<x-layout.header></x-layout.header>
+</html>
 
-<x-layout.footer></x-layout.footer>
+<body>
+    <div id="app">
+
+        <x-layout.header></x-layout.header>
+        @if (Config::get('app.debug') == 1)
+            <div class="debug-container">
+                <code>@yield('debugcontent')</code>
+            </div>
+        @endif
+
+        @yield('content')
+        <x-layout.footer></x-layout.footer>
+
+    </div>
+</body>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
     plugins: [
@@ -7,5 +8,13 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "resources/images",
+                    dest: ""
+                }
+            ]
+        })
     ],
 });
