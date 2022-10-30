929faf67b53b470e2af5d2148174c494a41f2eb7
Alyxia Sother <alyxia@riseup.net>
2022-10-30 09:22:19 +0000

    [Helpers/Icons] Allow fetching local SVGs

--- app/Helpers/Icons.php
+++ app/Helpers/Icons.php
@@ -3,16 +3,30 @@
 namespace App\Helpers;
 
 use DirectoryIterator;
+use Feather\Exception\IconNotFoundException;
+use Illuminate\Support\Facades\File;
 
 class Icons
 {
-    // TODO: Implement searching for SVGs in the images folder.
     public static function get(string $name, string $class = null)
     {
-        $icons = app('icons');
-        if (isset($class)) {
-            $icons->addCssClass($class);
+        try {
+            $icons = app('icons');
+            if (isset($class)) {
+                $icons->addCssClass($class);
+            }
+            return $icons->getIcon($name)->setAttribute('height', '')->setAttribute('width', '');
+        } catch (IconNotFoundException $e) {
+            $dir = new DirectoryIterator(dirname(__FILE__) . "/../../resources/images");
+            foreach ($dir as $file) {
+                $filename = $file->getFilename();
+                $filename = explode(".", $filename);
+                if ($name == $filename[0]) {
+                    $content = File::get($file->getRealPath());
+                    $svg = preg_replace("/<svg (\w+='.*')/", "<svg $1 class='{$class}'", $content);
+                    return $svg;
+                }
+            }
         }
-        return $icons->getIcon($name)->setAttribute('height', '')->setAttribute('width', '');
     }
 }
