ef39ba63d8d2aff3d402256f17b0edeac68c1e10
lleyton <lleyton@fyralabs.com>
2022-10-31 21:15:47 +0000

    Add flag constants

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,125 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
+     * Developer status. Public
+     */
+    const FLAG_DEVELOPER = 1 << 0;
+    /**
+     * Admin status. Public
+     */
+    const FLAG_ADMIN = 1 << 1;
+    /**
+     * Staff status. Public
+     */
+    const FLAG_STAFF = 1 << 2;
+    /**
+     * Moderator status. Public
+     */
+    const FLAG_MODERATOR = 1 << 3;
+    /**
+     * Support status. Public
+     */
+    const FLAG_SUPPORT =  1 << 4;
+    /**
+     * Contributor status. Public
+     */
+    const FLAG_CONTRIBUTOR = 1 << 5;
+    /**
+     * Translator status. Public
+     */
+    const FLAG_TRANSLATOR = 1 << 6;
+    /**
+     * Bug hunter status. Public
+     */
+    const FLAG_BUG_HUNTER = 1 << 7;
+    /**
+     * Early user status. Public
+     */
+    const FLAG_EARLY_USER = 1 << 8;
+
+    /**
+     * User donated at least once. Public.
+     */
+    const FLAG_HAS_DONATED = 1 << 9;
+    /**
+     * User is currently a Powercord Cutie. Public.
+     */
+    const FLAG_IS_CUTIE = 1 << 10;
+    /**
+     * Status has been manually set by a staff. Private.
+     */
+    const FLAG_CUTIE_OVERRIDE = 1 << 11;
+
+    /**
+     * User currently has a published item in the store. Public.
+     */
+    const FLAG_STORE_PUBLISHER = 1 << 12;
+    /**
+     * User has at least one verified item in the store. Public.
+     */
+    const FLAG_VERIFIED_PUBLISHER = 1 << 13;
+
+    /**
+     * User is banned from logging in. Private.
+     */
+    const FLAG_BANNED = 1 << 14;
+    /**
+     * User is banned from publishing in the store. Private.
+     */
+    const FLAG_BANNED_PUBLISHER = 1 << 15;
+    /**
+     * User is banned from requesting verification. Private.
+     */
+    const FLAG_BANNED_VERIFICATION = 1 << 16;
+    /**
+     * User is banned from requesting hosting. Private.
+     */
+    const FLAG_BANNED_HOSTING = 1 << 17;
+    /**
+     * User is banned from submitting reports. Private.
+     */
+    const FLAG_BANNED_REPORTING = 1 << 18;
+    /**
+     * User is banned from using Sync. Private.
+     */
+    const FLAG_BANNED_SYNC = 1 << 19;
+    /**
+     * User is banned from participating in community events. Private.
+     */
+    const FLAG_BANNED_EVENTS = 1 << 20;
+
+    /**
+     * User appealed a support ban. Private.
+     */
+    const FLAG_APPEALED_SUPPORT = 1 << 21;
+    /**
+     * User appealed a server mute. Private.
+     */
+    const FLAG_APPEALED_MUTE = 1 << 22;
+    /**
+     * User appealed a server ban. Private.
+     */
+    const FLAG_APPEALED_BAN = 1 << 23;
+    /**
+     * User appealed a Sync ban. Private.
+     */
+    const FLAG_APPEALED_SYNC = 1 << 24;
+    /**
+     * User appealed a community events ban. Private.
+     */
+    const FLAG_APPEALED_EVENTS = 1 << 25;
+
+    /**
+     * User is a ghost entry (entry with no real user data, used for flag keeping purposes). Private.
+     */
+    const FLAG_GHOST = 1 << 26;
+
+    /**
+     * User has boosted the Discord server. Public.
+     */
+    const FLAG_SERVER_BOOSTER = 1 << 27;
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
