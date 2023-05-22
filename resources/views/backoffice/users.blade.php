4ac38a466cddf150a796070d41c5a97b3d3ed5b4
Alyxia Sother <alyxia@riseup.net>
2023-05-22 10:29:09 -0400

    [views/backoffice/users] Update button href and disable on error

--- resources/views/backoffice/users.blade.php
+++ resources/views/backoffice/users.blade.php
@@ -27,6 +27,12 @@
             padding: 4px 8px;
             border-radius: 4px;
         }
+
+        .disabled {
+            cursor: not-allowed;
+            pointer-events: none;
+            filter: grayscale(70%);
+        }
     </style>
 @endpush
 
@@ -43,13 +49,21 @@
 @section('b-content')
     <h1 class="title">Manage users</h1>
     {{-- TODO(lexisother): Any nicer way than using `$watch`? --}}
-    <div x-data="{ value: '', err: false }" x-init="$watch('value', v => err = validate(v))">
+    <div x-data="{
+        value: '',
+        err: false,
+        get href() {
+            return `/backoffice/users/${this.value}`;
+        }
+    }" x-init="$watch('value', v => err = validate(v))">
         <x-notice x-show="err" id="error-notice" color="red">
             Invalid input!
         </x-notice>
         <div class="toolbar">
             <input x-model="value" type="text" placeholder="User ID" class="text-field" id="id-input" />
-            <x-button id="submit-edit" to="{{ RoutePro::BACKOFFICE_USERS() }}">Edit User</x-button>
+            <x-button id="submit-edit" x-bind:class="{ disabled: err }" x-bind:href="href"
+                to="{{ RoutePro::BACKOFFICE_USERS() }}">Edit User
+            </x-button>
         </div>
     </div>
 @endsection
