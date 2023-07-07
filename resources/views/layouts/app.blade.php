efeddd75b75435f0c3c3031b8c04c2bd869d8fe2
Alyxia Sother <alyxia@riseup.net>
2023-07-07 14:02:18 +0200

    [views/store] Fix store styling by dropping TW

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,11 +28,6 @@
     @stack('head-js')
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    {{-- Terrbly old TW version, also a temporary solution because I'm too lazy to write proper styles right now --}}
-    @if(Request::is('store/*'))
-        <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" />
-    @endif
 </html>
 
 <body>
@@ -48,7 +43,7 @@
 
         @yield('content')
 
-        <div x-data class="absolute bottom-0 right-0 p-4 max-w-sm overflow-x-hidden">
+        <div x-data class="toast-container">
             <template
                 x-for="(toast, index) in $store.toasts.list"
                 :key="toast.id"
@@ -56,18 +51,18 @@
                 <div
                     x-show="toast.visible"
                     @click="$store.toasts.destroyToast(index)"
-                    x-transition:enter="transition ease-in duration-200"
-                    x-transition:enter-start="transform opacity-0 translate-y-2"
-                    x-transition:enter-end="transform opacity-100"
-                    x-transition:leave="transition ease-out duration-500"
-                    x-transition:leave-start="transform translate-x-0 opacity-100"
-                    x-transition:leave-end="transform translate-x-full opacity-0"
-                    class="bg-gray-900 bg-gradient-to-r text-white p-3 rounded mb-3 shadow-lg flex items-center"
+                    x-transition:enter="transition ease-in"
+                    x-transition:enter-start="transform enter-start"
+                    x-transition:enter-end="transform enter-end"
+                    x-transition:leave="transition ease-out"
+                    x-transition:leave-start="transform leave-start"
+                    x-transition:leave-end="transform leave-end"
+                    class="toast"
                     :class="{
-                        'from-blue-500 to-blue-600': toast.type === 'info',
-                        'from-green-500 to-green-600': toast.type === 'success',
-                        'from-yellow-400 to-yellow-500': toast.type === 'warning',
-                        'from-red-500 to-pink-500': toast.type === 'error'
+                        'info': toast.type === 'info',
+                        'success': toast.type === 'success',
+                        'warning': toast.type === 'warning',
+                        'error': toast.type === 'error'
                     }"
                 >
                     <div x-text="toast.message"></div>
